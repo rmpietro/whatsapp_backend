8cf5ed2ed074b122277e9f57ad2e766272c6b547
Rodrigo Mastropietro <rmpietro@gmail.com>
2024-05-26 23:22:49 -0300

    Setup cross-env adding middlewares

--- src/app.js
+++ src/app.js
@@ -1,10 +1,54 @@
 import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from "cors";
+
+//dotenv config
+dotenv.config();
 
 //Create Express App
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello from server!");
+//--------------Middlewares---------------- //
+
+//Morgan Middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+
+//Helmet Middleware
+app.use(helmet());
+
+//Parse Json request url middleware
+app.use(express.urlencoded({ extended: true }));
+
+//Parse Json request body middleware
+app.use(express.json());
+
+//Sanitize Request Data middleware
+app.use(mongoSanitize);
+
+//Enable Cookie Parser middleware
+app.use(cookieParser());
+
+//gzip compression middleware
+app.use(compression());
+
+//File upload middleware
+app.use(fileUpload({ useTempFiles: true }));
+
+//Cors Middleware
+app.use(cors());
+
+//-------------End middlewares-----------------//
+
+app.post("/test", (req, res) => {
+  res.send(req.body);
 });
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import app from "./app.js";
-import dotenv from "dotenv";
-
-//dotenv config
-dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 
